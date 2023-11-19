8510bc1342ed783a701cda18de86450cf2d58949
TanPT <>
2023-11-20 00:57:12 +0700

    update code

--- command.sh
+++ command.sh
@@ -1,9 +1,9 @@
 DB_USERNAME=postgres DB_PASSWORD=6bLJ8FcFyx python app.py
 
-    kubectl run udacity1-postgresql-client --rm --tty -i --restart='Never' --namespace default --image docker.io/bitnami/postgresql:16.1.0-debian-11-r6 --env="PGPASSWORD=$POSTGRES_PASSWORD" \
+kubectl run udacity1-postgresql-client --rm --tty -i --restart='Never' --namespace default --image docker.io/bitnami/postgresql:16.1.0-debian-11-r6 --env="PGPASSWORD=$POSTGRES_PASSWORD" \
       --command -- psql --host udacity1-postgresql -U postgres -d postgres -p 5432
 
-    kubectl port-forward --namespace default svc/udacity1-postgresql 5432:5432 &
+kubectl port-forward --namespace default svc/udacity1-postgresql 5432:5432 &
     PGPASSWORD="$POSTGRES_PASSWORD" psql --host 127.0.0.1 -U postgres -d postgres -p 5432
 
 kubectl exec --stdin --tty postgres-postgresql-0 -- /bin/bash
